642c4181537d321621ecbe1e0247be4626b766b6
Канай Раимкулов <kanay.raimkulov@gmail.com>
2024-02-14 15:16:11 +0600

    тестовое добавление

---
+++ SecondTech.Core/test/hi.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondTech.Core.test
+{
+    public class Hi
+    {
+        public static void Shmain()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
